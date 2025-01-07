de19ef9e34978d0cff641d32f25d3188dd8fac9a
Gabriel Chavez Pc <gabriel.chavez.r@gmail.com>
2025-01-07 06:09:53 -0400

    log

---
+++ UNIVidaPortalWeb.Common.Log/Src/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace UNIVidaPortalWeb.Common.Log.Src
+{
+    public static class Extensions
+    { 
+        private static readonly string SectionName = "logseq";
+        public static void UseLogSeq(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+
+                 IConfiguration configuration = scope.ServiceProvider.GetService<IConfiguration>();
+
+                var options = configuration.GetOptions<LogOptions>(SectionName);
+                //var options = scope.ServiceProvider.GetService<IOptions<LogOptions>>();
+
+                if (options.Enabled)
+                {
+                    var loggerFactory = scope.ServiceProvider.GetService<ILoggerFactory>();
+                    loggerFactory.AddSeq(options.Url, apiKey: options.Token);
+                }
+            }
+        }
+
+    }
+}
---
+++ UNIVidaPortalWeb.Common.Log/Src/LogOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace UNIVidaPortalWeb.Common.Log.Src
+{
+    public class LogOptions
+    {
+        public bool Enabled { get; set; }
+        public string Url { get; set; }
+        public string Token { get; set; }
+    }
+}
